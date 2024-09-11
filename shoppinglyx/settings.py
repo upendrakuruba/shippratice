034b84fe866925a60252169e04c0d7f104b62331
upendraupi <upendrakuruma@gmail.com>
2024-09-11 09:40:21 +0530

    settings changes

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8o353)z(_*^04+jhey!m*q3_no@vhn*7v8(6xz4rl6=kunu=it'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
